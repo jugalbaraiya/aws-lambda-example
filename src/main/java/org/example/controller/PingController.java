d655a9c9f9ff179eaaedb7b836a00a78c02bccaa
Jugal Baraiya <jugalbaraiya@gmail.com>
2024-12-22 20:03:04 +0530

    Test commit

--- src/main/java/org/example/controller/PingController.java
+++ src/main/java/org/example/controller/PingController.java
@@ -14,7 +14,7 @@ public class PingController {
     @RequestMapping(path = "/ping", method = RequestMethod.GET)
     public Map<String, String> ping() {
         Map<String, String> pong = new HashMap<>();
-        pong.put("pong", "Hello, World!");
+        pong.put("pong", "Hello, World !!!");
         return pong;
     }
 }
